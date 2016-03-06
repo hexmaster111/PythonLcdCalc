441ee04b02e0f2013e1ba1acb962382707adfe7e
Hexmaster111 <hexmaster111@gmail.com>
2016-03-06 15:14:27 -0600

    what chaged?

--- printinput.py
+++ printinput.py
@@ -12,22 +12,7 @@ GPIO.setup(LED_ON,GPIO.OUT)
 time.sleep(DELAY)
 ##### INIT BLOCK END #####
 
-def main():
-	print("ProgRan")
+lcd.lcd_string("this is text", 1)
 
-def lcd_pipe():
-	#Text to be Wirghten to eatch line
-	LineOne = "Test 1"
-	LineTwo = "Test 2"
-	LineThree = "Test 3"
-	LineFour = "Test $"
 
-	##Print the text to each line
-	lcd.lcd_byte(lcd.LCD_LINE_1, lcd.LCD_CMD)
-	lcd.lcd_string(LineOne, 1)
-	lcd.lcd_byte(lcd.LCD_LINE_2, lcd.LCD_CMD)
-	lcd.lcd_string(LineTwo, 1)
-	lcd.lcd	_byte(lcd.LCD_LINE_3, lcd.LCD_CMD)
-	lcd.lcd_string(LineThree, 1)
-	lcd.lcd_byte(lcd.LCD_LINE_4, lcd.LCD_CMD)
-	lcd.lcd_string(LineFour, 1)
+
